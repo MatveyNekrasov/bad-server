27172a539c68b5759ed279602a7a6b032f638f4c
Matvey Nekrasov <matvei-n@mail.ru>
2025-01-20 19:26:05 +0300

    file metadata

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
-import { checkMinFileSize, upload } from '../middlewares/file'
+import { checkImageContent, checkMinFileSize, upload } from '../middlewares/file'
 
 const uploadRouter = Router()
-uploadRouter.post('/', checkMinFileSize, upload.single('file'), uploadFile)
+uploadRouter.post('/', checkMinFileSize, checkImageContent, upload.single('file'), uploadFile)
 
 export default uploadRouter
